cdc4d0ed391f699b9ae8ab3edee41b6bf6ea8446
Anshul Negi <56960100+Anshul1310@users.noreply.github.com>
2026-01-18 09:41:38 +0530

    backend

--- src/lib/db.js
+++ src/lib/db.js
@@ -6,7 +6,10 @@ const connectToDb= async()=>{
     try{
         console.log(process.env.MONGODB_URI)
         await mongoose.connect(process.env.MONGODB_URI)
+        console.log("connected to db")
     }catch(e){
         console.log(e)
     }
 }
+
+export default connectToDb
